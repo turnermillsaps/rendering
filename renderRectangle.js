22632a6248324338733693181f31c8f53338dc5e
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-02-27 22:49:20 -0500

    Finished nametags, rectangles, and students

--- renderRectangle.js
+++ renderRectangle.js
@@ -1,10 +1,14 @@
 
 function renderRectangle(rectangle) {
-    return `
+    /* return `
         <div class="text-center mt-5">
             <code>${JSON.stringify(rectangle)}</code>
         </div>
+    ` */
+    var retRectangle = `
+        <div style="width: ${rectangle.width}px; height: ${rectangle.height}px; background-color: ${rectangle.color}"></div>
     `
+    return retRectangle;
 }
 
 function rectangle() {
