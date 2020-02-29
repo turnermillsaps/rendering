5e74432ff99f7a1c6f3ac6caf97202aaada8f03e
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-02-29 12:04:33 -0500

    Finished renderRestaurants

--- renderRestaurants.js
+++ renderRestaurants.js
@@ -2,11 +2,38 @@
 function renderRestaurants(restaurants) {
     // HINT: You can use <img /> tags that point to these playing card images: 
     // https://commons.wikimedia.org/wiki/Category:SVG_playing_cards
-    return `
+    /* return `
         <div class="text-center mt-5">
             <code>${JSON.stringify(restaurants)}</code>
         </div>
-    `
+    ` */
+
+    var restArr = restaurants.map(function(e){
+        var dollarSign = "";
+        for (var i = 0; i < e.priceRating; i++) {
+            dollarSign += "$";
+        }
+
+        return `
+                    <div class="card col-3 m-2">
+                        <div class="card-body">
+                            <h2 class="card-title">${e.name}</h2>
+                            <p class="card-text">${e.type}</p>
+                            <p class="card-text" style="color: green;">${dollarSign}</p>
+                        </div>
+                    </div>
+        `
+    }) 
+
+    var restCards = restArr.join('');
+    var restContainer = `
+        <div class="container-fluid">
+            <div class="row">${restCards}</div>
+        </div>
+    `;
+    return restContainer;
+    
+   
 }
 
 function restaurants() {
